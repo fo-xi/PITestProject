4d7af7a0905327277ad49fdea5186ebcffdf099d
Дягай Александра <dyagaay@gmail.com>
2024-05-17 06:54:59 +0300

    изменения

--- src/test/java/MatrixMultiplicationTest.java
+++ src/test/java/MatrixMultiplicationTest.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MatrixMultiplicationTest {
     private MatrixMultiplication matrixMultiplication;
